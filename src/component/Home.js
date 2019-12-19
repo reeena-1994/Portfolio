1e6a1b2d032be81645519b9a6488c3ddadaf2187
reina.kato <reina.kato@eras.jp>
2019-12-19 12:11:12 +0900

    https://github.com/reeena-1994/Portfolio.git

---
+++ src/component/Home.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Home.css';
+import img from './background.jpeg';
+import { Link, } from 'react-router-dom'
+
+class Home extends React.Component {
+
+  render(){
+    const style = {
+      linkLine: {textDecoration: 'none'},
+    }
+    return(
+        <div className="home__main">
+            <div className="My-portfolio">
+                <img src={img} className="Home-img" alt="logo" />
+                <div className="About">
+                  <Link style={style.linkLine} to="/About"><div className="About-text">About</div></Link>
+                </div>
+                <div className="Work">
+                  <Link style={style.linkLine} to="/Work"><div className="Work-text">Work</div></Link>
+                </div>
+                <div className="Contact">
+                  <Link style={style.linkLine} to="/Contact"><div className="Contact-text">Contact</div></Link>
+                </div>
+            </div>
+        </div>
+    )
+  }
+}
+export default Home;
