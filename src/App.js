6a2f2db02590bb08bf8abe4e8323c56ef016ab46
reina.kato <reina.kato@eras.jp>
2019-12-19 23:41:35 +0900

    https://github.com/reeena-1994/Portfolio.git

--- src/App.js
+++ src/App.js
@@ -10,29 +10,29 @@ class App extends Component {
   render() {
     return (
         <div className="demo-big-content">
-            <Layout>
-                <div className="header">
-                    <Navigation>
-                    </Navigation>
-                </div>
-                <Drawer>
-                    <Navigation>
-                    <Link to="/">Home</Link>
-                    <Link to="/About">About</Link>
-                    <Link to="/Work">Work</Link>
-                    <Link to="/Contact">Contact</Link>
-                    </Navigation>
-                </Drawer>
-            </Layout>
             <HashRouter basename = '/'>
+                <Layout>
+                    <div className="header">
+                        <Navigation>
+                        </Navigation>
+                    </div>
+                    <Drawer>
+                        <Navigation>
+                        <Link to="/">Home</Link>
+                        <Link to="/About">About</Link>
+                        <Link to="/Work">Work</Link>
+                        <Link to="/Contact">Contact</Link>
+                        </Navigation>
+                    </Drawer>
+                </Layout>
                 <Route exact path={'/'} component={Home}/>
-                    <Route path={'/about'}  component={About}/>
-                    <Route path={'/work'} component={Work}/>
-                    <Route path={'/contact'} component={Contact}/>
+                <Route exact path={'/about'}  component={About}/>
+                <Route exact path={'/work'} component={Work}/>
+                <Route exact path={'/contact'} component={Contact}/>
             </ HashRouter >
         </div>
-  );
-}
+     );
+  }
 }
 
 export default App;
