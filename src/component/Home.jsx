d044007e5c4faa7839afec1dd87af6eabf4cf932
reina.kato <reina.kato@eras.jp>
2020-01-06 18:32:49 +0900

    hennkoudayo

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -14,25 +14,22 @@ class Home extends React.Component {
             <div className="My-portfolio">
                 <img src={img} className="Home-img" alt="logo" />
                 <Link style={style.linkLine} to="/About">
+                  <div className="About-border">
                     <div className="About">
-                    <div className="About-border">
-
                       <div className="About-text">About</div>
                     </div>
                   </div>
                 </Link>
                 <Link style={style.linkLine} to="/Work">
+                  <div className="Work-border">
                     <div className="Work">
-                    <div className="Work-border">
-
                       <div className="Work-text">Work</div>
                     </div>
                   </div>
                 </Link>
                 <Link style={style.linkLine} to="/Contact">
+                  <div className="Contact-border">
                     <div className="Contact">
-                    <div className="Contact-border">
-
                       <div className="Contact-text">Contact</div>
                     </div>
                   </div>
