a57f1edf4db703c8b2ae5afe7c9b63959d67e595
reina.kato <reina.kato@eras.jp>
2020-01-07 16:50:21 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.06898e01530943605f4de3923fd888b2.js"
+  "/Portfolio/precache-manifest.61faa192a564f82da790c7a340d0e50a.js"
 );
 
 self.addEventListener('message', (event) => {
