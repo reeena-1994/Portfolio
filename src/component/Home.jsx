3b9fb681c8d82d801037e42e81955d244b3905b2
reina.kato <reina.kato@eras.jp>
2020-01-07 10:56:29 +0900

    henkou

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -13,27 +13,27 @@ class Home extends React.Component {
         <div className="home__main">
             <ul className="My-portfolio">
                 <img src={img} className="Home-img" alt="logo" />
-                <Link style={style.linkLine} to="/About">
-                  <li className="About-border">
-                    <div className="About">
-                      <div className="About-text">About</div>
-                    </div>
-                  </li>
-                </Link>
-                <Link style={style.linkLine} to="/Work">
-                  <li className="Work-border">
-                    <div className="Work">
-                      <div className="Work-text">Work</div>
-                    </div>
-                  </li>
-                </Link>
-                <Link style={style.linkLine} to="/Contact">
-                  <li className="Contact-border">
-                    <div className="Contact">
-                      <div className="Contact-text">Contact</div>
-                    </div>
-                  </li>
-                </Link>
+                <li className="About-border">
+                  <Link style={style.linkLine} to="/About">
+                      <div className="About">
+                        <div className="About-text">About</div>
+                      </div>
+                  </Link>
+                </li>
+                <li className="Work-border">
+                  <Link style={style.linkLine} to="/Work">
+                      <div className="Work">
+                        <div className="Work-text">Work</div>
+                      </div>
+                  </Link>
+                </li>
+                <li className="Contact-border">
+                  <Link style={style.linkLine} to="/Contact">
+                      <div className="Contact">
+                        <div className="Contact-text">Contact</div>
+                      </div>
+                  </Link>
+                </li>
             </ul>
         </div>
     )
